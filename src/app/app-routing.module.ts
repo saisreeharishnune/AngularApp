4e5b03ce3691b4d6a1e9190ad4e3cba465e974e1
saisreeharishnune <nune.harish@gmail.com>
2021-11-29 21:56:04 +0530

    added Login Component and Login Routing Entry

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SignupComponent } from './signup/signup.component';
 
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [{path:'welcome',component:WelcomeComponent},
+                        {path:'login',component:LoginComponent},
                         {path:'signup',component:SignupComponent},
                         {path:'', redirectTo:'welcome',pathMatch:'full'},
                         {path:'**', component:PageNotFoundComponent}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm:FormGroup = this.fb.group({
+    userName:[''],
+    password:['']
+  });
+  constructor(private fb:FormBuilder,private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  login(){
+    console.log(this.loginForm.value);
+    
+    this.router.navigateByUrl('/welcome');
   }
 
 }
